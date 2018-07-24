ca4ea2e1aa5b1f32ee28cb2b1daabca6afdceba6
Brian Gibson <bggibson@hotmail.com>
2018-07-24 18:07:25 -0400

    Modifies AmazonClient Modifies AmazonClientTest

--- src/main/java/com/ericgibson/website/code/AmazonClient.java
+++ src/main/java/com/ericgibson/website/code/AmazonClient.java
@@ -1,8 +1,9 @@
 package com.ericgibson.website.code;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 
 import java.io.File;
 import java.nio.file.Paths;
@@ -17,17 +18,9 @@ class AmazonClient {
     }
 
     Bucket createBucket(String bucketName) {
-        Bucket bucket = null;
-        if (s3.doesBucketExistV2(bucketName)) {
-            bucket = getBucket(bucketName);
-        } else {
-            try {
-                bucket = s3.createBucket(bucketName);
-            } catch (AmazonS3Exception e) {
-                System.err.println(e.getErrorMessage());
-            }
-        }
-        return bucket;
+        if (s3.doesBucketExistV2(bucketName))
+            return getBucket(bucketName);
+        return s3.createBucket(bucketName);
     }
 
     private Bucket getBucket(String bucketName) {
@@ -43,15 +36,7 @@ class AmazonClient {
 
     PutObjectResult putObject(String bucketName, String filePath) {
         String fileName = Paths.get(filePath).getFileName().toString();
-        PutObjectResult putObjectResult = null;
-        try {
-//            PutObjectRequest request = new PutObjectRequest(bucketName, fileName, new File(filePath)).withCannedAcl(CannedAccessControlList.PublicReadWrite);
-            PutObjectRequest request = new PutObjectRequest(bucketName, fileName, new File(filePath));
-            putObjectResult = s3.putObject(request);
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            System.exit(1);
-        }
-        return putObjectResult;
+        PutObjectRequest request = new PutObjectRequest(bucketName, fileName, new File(filePath));
+        return s3.putObject(request);
     }
 }
