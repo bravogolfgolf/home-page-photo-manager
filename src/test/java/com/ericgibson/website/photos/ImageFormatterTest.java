3d7b79e501d701c37d400ca32145af918f7caeff
Brian Gibson <bggibson@hotmail.com>
2018-08-02 10:45:00 -0400

    Modifies ImageFormatterTest

--- src/test/java/com/ericgibson/website/photos/ImageFormatterTest.java
+++ src/test/java/com/ericgibson/website/photos/ImageFormatterTest.java
@@ -2,9 +2,13 @@ package com.ericgibson.website.photos;
 
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class ImageFormatterTest {
 
@@ -12,14 +16,15 @@ public class ImageFormatterTest {
     private final File file = new File("IMG_0574.jpg");
 
     @Test
-    public void shouldCreatePhotoManipulate() {
+    public void shouldCreateThumbnailPreservingRatio() throws IOException {
         File thumbnail = imageFormatter.createThumbnail(file);
-        assertThat(thumbnail.length()).isLessThan(file.length());
+        BufferedImage fromFileImage = ImageIO.read(thumbnail);
+        assertThat(fromFileImage.getWidth()).isEqualTo(200);
         thumbnail.delete();
     }
 
     @Test
-    public void shouldCreateSameSizePhotoWithProperOrientationMetadata() {
+    public void shouldModifyPhotoToHaveProperOrientation() {
 //      Not sure how to test...
         imageFormatter.setOrientation(file);
     }
