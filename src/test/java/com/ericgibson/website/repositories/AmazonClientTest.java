0ef93608011eb6016b32fec41dd6e46e3fc4f896
Brian Gibson <bggibson@hotmail.com>
2021-06-21 08:04:30 -0600

    Remove create/destroy for test bucket because aws user permissions narrowed

--- src/test/java/com/ericgibson/website/repositories/AmazonClientTest.java
+++ src/test/java/com/ericgibson/website/repositories/AmazonClientTest.java
@@ -4,7 +4,6 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.ericgibson.website.gateways.CloudStorageGateway;
-import org.junit.After;
 import org.junit.Test;
 
 import java.util.List;
@@ -20,15 +19,8 @@ public class AmazonClientTest {
 
     private final CloudStorageGateway gateway = new AmazonClient(amazonS3);
 
-    @After
-    public void teardown() {
-        amazonS3.listObjectsV2(STORAGE).getObjectSummaries().forEach((item) -> amazonS3.deleteObject(STORAGE, item.getKey()));
-        amazonS3.deleteBucket(STORAGE);
-    }
-
     @Test
     public void shouldTestCompleteStorageLifecycle() {
-        gateway.createStorage(STORAGE);
         assertThat(amazonS3.doesBucketExistV2(STORAGE)).isTrue();
         assertThat(gateway.listObjectKeys(STORAGE)).isEmpty();
         gateway.putObject(STORAGE, KEY, FILE);
